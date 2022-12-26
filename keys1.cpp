c40a50d9ab35fd7b0253539cf5162abb2c245d7c
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 15:12:25 +0300

    Update keys1.cpp

--- keys1.cpp
+++ keys1.cpp
@@ -39,3 +39,17 @@ if (a1<0){a1=-1*a1;}
     }
     return a2;
 }
+double itc_pow(int a1, int a2) {
+    double a3;
+    a3=1;
+    long long a4;
+    if (a2<0)
+        a4=-1*a2;
+    else
+        a4=a2;
+    for (long long i=0;i<a4;++i)
+        a3*=a1;
+    if (a2<0)
+        a3=1/a3;
+    return a3;
+}

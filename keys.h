e07aa92544c2978aeb1a2cf23fbce120852dd300
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 15:11:56 +0300

    Update keys.h

--- keys.h
+++ keys.h
@@ -5,5 +5,5 @@ using namespace std;
 long long itc_reverse_num(long long a1);
 long long itc_bin_num(long long a1);
 int itc_len_num(long long a1);
-
+double itc_pow(int a1, int a2);
 #endif // KEYS_H_INCLUDED
